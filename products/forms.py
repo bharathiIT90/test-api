2dc9761f142b96dd7aff9da8c4f010425759369c
Bharathi <bharathiranganathan24@gmail.com>
2016-07-16 16:36:16 -0700

    Adding social sharing links

--- products/forms.py
+++ products/forms.py
@@ -8,7 +8,7 @@ class VariationInventoryForm(forms.ModelForm):
 	class Meta:
 		model = Variation
 		fields = [
-			"title",
+			
 			"price",
 			"sale_price",
 			"inventory",
@@ -17,6 +17,6 @@ class VariationInventoryForm(forms.ModelForm):
 
 
 
-VariationInventoryFormSet = modelformset_factory(Variation, form =  VariationInventoryForm, extra = 1)
+VariationInventoryFormSet = modelformset_factory(Variation, form =  VariationInventoryForm, extra = 0)
 
 
