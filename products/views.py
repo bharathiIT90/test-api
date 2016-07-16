1af1d39c65aebaf1f36ff5ecef643ca618aaa831
Bharathi <bharathiranganathan24@gmail.com>
2016-07-16 13:00:16 -0700

    Django messages

--- products/views.py
+++ products/views.py
@@ -30,15 +30,16 @@ class VariationListView(LoginRequiredMixin,ListView):
 	def post(self,request, *args, **kwargs):
 		#
 		formset = VariationInventoryFormSet(request.POST, request.FILES)
-		print request.POST
+		
 		if formset.is_valid():
 			formset.save(commit = False)
 			for form in formset:
 				new_item = form.save(commit = False)
-				product_pk = self.kwargs.get("pk")
-				product = get_object_or_404(Product, pk=product_pk)
-				new_item.product = product
-				new_item.save()
+				if new_item.title:
+					product_pk = self.kwargs.get("pk")
+					product = get_object_or_404(Product, pk=product_pk)
+					new_item.product = product
+					new_item.save()
 
 			messages.success(request, "Your inventory and pricing has been updated.")
 			return redirect("products")
