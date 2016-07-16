ab9b2e1319d81ca7de334165cf109dd54da60d48
Bharathi <bharathiranganathan24@gmail.com>
2016-07-16 12:16:18 -0700

    Creating forms.py and formset for inventory

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from.views import ProductDetailView,ProductListView
+from.views import ProductDetailView,ProductListView, VariationListView
 
 
 urlpatterns = [
@@ -12,6 +12,7 @@ urlpatterns = [
 	#url(r'^$', 'newsletter.views.home', name='home'),
 	url(r'^$', ProductListView.as_view(), name='products'),
 	url(r'^(?P<pk>\d+)/$', ProductDetailView.as_view(), name='product_detail'),
+	url(r'^(?P<pk>\d+)/inventory/$', VariationListView.as_view(), name='product_inventory'),
 	
 	#url(r'^(?P<id>\d+)', 'products.views.product_detail_view_func', name='product_view_function')
 
