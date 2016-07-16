7daa730b802aa0c14e4477d04f2ebaaff0ddb63a
Bharathi <bharathiranganathan24@gmail.com>
2016-07-15 19:26:18 -0700

    Image Upload

---
+++ products/migrations/0003_auto_20160716_0202.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('products', '0002_variation'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='ProductImage',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('image', models.ImageField(upload_to=b'products/')),
+                ('product', models.ForeignKey(to='products.Product')),
+            ],
+        ),
+        migrations.AlterField(
+            model_name='variation',
+            name='inventory',
+            field=models.IntegerField(null=True, blank=True),
+        ),
+    ]
