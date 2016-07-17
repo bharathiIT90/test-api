c6f8d0264cbc2de93eaf2917bf4269fed7bbc0cb
Bharathi <bharathiranganathan24@gmail.com>
2016-07-16 18:16:11 -0700

    Adding product categories

--- products/models.py
+++ products/models.py
@@ -21,6 +21,8 @@ class Product(models.Model):
 	description = models.TextField(blank=True,null=True)
 	price = models.DecimalField(decimal_places=2,max_digits=20)
 	active = models.BooleanField(default=True)
+	categories = models.ManyToManyField('Category', blank = True)
+	default = models.ForeignKey('Category', related_name = 'default_category', null=True, blank = True)
 	#slug
 	#inventory?
 
@@ -84,3 +86,17 @@ class ProductImage(models.Model):
 		return self.product.title
 #Product Images
 #Product Category
+
+
+class Category(models.Model): 
+	title = models.CharField(max_length=120)
+	slug = models.SlugField(unique = True)
+	description = models.TextField(null = True, blank = True)
+	active =  models.BooleanField(default = True)
+	timestamp = models.DateTimeField(auto_now_add = True, auto_now = False)
+
+	def __unicode__(self):
+		return self.title
+
+
+
