6836bb1fb5f10a300183b0ea6c138067d6c019c0
Bharathi <bharathiranganathan24@gmail.com>
2016-07-17 21:07:59 -0700

    Editing products using admin

--- products/admin.py
+++ products/admin.py
@@ -4,8 +4,31 @@ from django.contrib import admin
 
 from .models import Product,Variation, ProductImage, Category, ProductFeatured
 
-admin.site.register(Product)
-admin.site.register(Variation)
+class ProductImageInline(admin.TabularInline):
+	model = ProductImage
+	extra = 0
+	max_num = 10
+
+class VariationInline(admin.TabularInline):
+	model = Variation
+	extra = 0 
+	max_num = 10
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ['__unicode__','price']
+	inlines = [
+		VariationInline,
+		ProductImageInline,
+		
+	]
+	class Meta:
+		model = Product
+
+admin.site.register(Product, ProductAdmin)
+
+#admin.site.register(Variation)
 
 admin.site.register(ProductImage)
 
