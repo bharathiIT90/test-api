60da17e295052700179df0ae103363a91922a43f
Bharathi <bharathiranganathan24@gmail.com>
2016-07-19 10:55:45 -0700

    Adding Ajax for adding item in cart

--- carts/views.py
+++ carts/views.py
@@ -1,60 +1,60 @@
-from django.views.generic.base import View
+from django.http import HttpResponseRedirect, Http404, JsonResponse
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect, Http404
+from django.views.generic.base import View
 from django.views.generic.detail import SingleObjectMixin
+
+
 # Create your views here.
+
 from products.models import Variation
 from carts.models import Cart, CartItem
-
-
-
 
 class CartView(SingleObjectMixin, View):
 	model = Cart
 	template_name = "carts/view.html"
 
-	def get_object(self,*args,**kwargs):
-		self.request.session.set_expiry(0)
+	def get_object(self, *args, **kwargs):
+		self.request.session.set_expiry(0) #5 minutes
 		cart_id = self.request.session.get("cart_id")
 		if cart_id == None:
 			cart = Cart()
 			cart.save()
 			cart_id = cart.id
 			self.request.session["cart_id"] = cart_id
-		
 		cart = Cart.objects.get(id=cart_id)
 		if self.request.user.is_authenticated():
 			cart.user = self.request.user
 			cart.save()
 		return cart
 
-	def get(self,request,*args,**kwargs):
+	def get(self, request, *args, **kwargs):
 		cart = self.get_object()
 		item_id = request.GET.get("item")
-		delete_item = request.GET.get("delete")
-		print delete_item
+		delete_item = request.GET.get("delete", False)
+		item_added = False
 		if item_id:
-			item_instance = get_object_or_404(Variation,id=item_id)
-			qty = request.GET.get("qty",1)
+			item_instance = get_object_or_404(Variation, id=item_id)
+			qty = request.GET.get("qty", 1)
 			try:
-				if int(qty) <  1:
+				if int(qty) < 1:
 					delete_item = True
 			except:
 				raise Http404
-
-			cart_item = CartItem.objects.get_or_create(cart=cart,item=item_instance)[0]
+			cart_item, created = CartItem.objects.get_or_create(cart=cart, item=item_instance)
+			if created:
+				item_added = True
 			if delete_item:
 				cart_item.delete()
 			else:
 				cart_item.quantity = qty
 				cart_item.save()
+		
+		if request.is_ajax():
+			return JsonResponse({"deleted": delete_item, "item_added": item_added}) 
+
+
 		context = {
-			"object":self.get_object()
+			"object": self.get_object()
 		}
 		template = self.template_name
-		return render(request,template,context)
-
-
-
-			
-		
+		return render(request, template, context)
