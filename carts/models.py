0b8f4134052e5ba9934caa3ac75aca6311f51b47
Bharathi <bharathiranganathan24@gmail.com>
2016-07-18 22:29:21 -0700

    Updating Cart Item total

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,7 @@
+from decimal import Decimal
 from django.conf import settings
 from django.core.urlresolvers import reverse
+from django.db.models.signals import pre_save
 from django.db import models
 from products.models import Variation
 # Create your models here.
@@ -8,7 +10,7 @@ class CartItem(models.Model):
 	cart = models.ForeignKey("Cart")
 	item = models.ForeignKey(Variation)
 	quantity = models.PositiveIntegerField(default =1)
-	#line item total
+	line_item_total = models.DecimalField(max_digits=10, decimal_places=2)
 
 
 	def __unicode__(self):
@@ -17,9 +19,16 @@ class CartItem(models.Model):
 	def remove(self):
 		return  self.item.remove_from_cart()
 
-	def get_title(self):
-		return "%s-%s" %(self.item.product.title, self.item.title)
 
+def cart_item_pre_save_receiver(sender,instance, *args,**kwargs):
+	qty = instance.quantity
+	if qty >=1:
+		price = instance.item.get_price()
+		line_item_total = Decimal(qty) * Decimal(price)
+		instance.line_item_total=line_item_total
+
+
+pre_save.connect(cart_item_pre_save_receiver,sender = CartItem)
 
 class Cart(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL,null=True, blank=True)
